c710bfc4c7a9c48d4988d4a4b827da677a16182a
Marc Freudenberg <marc.freudenberg@mobilityhouse.com>
2023-01-15 21:47:22 +0100

    more mqtt config

--- cmd/edgedb-ingest/edgedb-ingest.go
+++ cmd/edgedb-ingest/edgedb-ingest.go
@@ -21,6 +21,27 @@ func getMQTTOpts() *MQTT.ClientOptions {
 	mqttOpts.AddBroker(os.Getenv("MQTT_BROKER_URL"))
 	mqttOpts.SetUsername(os.Getenv("MQTT_BROKER_USERNAME"))
 	mqttOpts.SetPassword(os.Getenv("MQTT_BROKER_PASSWORD"))
+
+	mqttOpts.SetOrderMatters(false)
+	mqttOpts.ConnectTimeout = time.Second
+	mqttOpts.WriteTimeout = time.Second
+	mqttOpts.KeepAlive = 10
+	mqttOpts.PingTimeout = time.Second
+	mqttOpts.ConnectRetry = true
+	mqttOpts.AutoReconnect = true
+
+	mqttOpts.DefaultPublishHandler = func(_ MQTT.Client, msg MQTT.Message) {
+		log.Warn().Interface("msg", msg).Msg("unexpected message")
+	}
+
+	mqttOpts.OnConnectionLost = func(cl MQTT.Client, err error) {
+		log.Err(err).Msg("MQTT connection lost")
+	}
+
+	mqttOpts.OnReconnecting = func(MQTT.Client, *MQTT.ClientOptions) {
+		log.Warn().Msg("MQTT attempting to reconnect")
+	}
+
 	return mqttOpts
 }
 
